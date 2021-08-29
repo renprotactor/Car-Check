306adb6811a365b4aa769091b74a012954e6e67d
rmohan <renju_k_m@hotmail.com>
2021-08-29 17:38:59 +0100

    Runner Updated

--- src/test/java/com/carcheck/runner/TestRunner.java
+++ src/test/java/com/carcheck/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
     @CucumberOptions(features = {"classpath:features/FreeCarCheck.feature"},
             glue = {"com.carcheck.stepDefs"},
-       plugin = {"pretty", "html:src/test/java/com/carcheck/reports/test-output.html", "json:src/test/java/com/carcheck/reports/cucumber.json"})
+       plugin = {"pretty", "html:target/reports/test-output.html"})
 //            plugin = {"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"})
 
     public class TestRunner {
